faae87789d7f245751307c0486775dbf5b01201e
NotRynn <148168730+NotRynn@users.noreply.github.com>
2026-09-02 13:08:30 -0700

    0.1

---
+++ AboutMe.java
@@ -0,0 +1,12 @@
+public class AboutMe {
+    public static void main(String[] args) {
+
+        System.out.println("Name: Gleb Shaludkin\n" + //
+                "Birthday: October 1st 2010\n" + //
+                "Hobbies: Reading, Writing, Skateboarding\n" + //
+                "Favorite Book: The State and The Revolution\n" + //
+                "Favorite Movie: Spider-Man: Accross the Spider-Verse / Spider-Man: Brand New Day\n"
+                + //
+                "Favorite Quote: \"While the State exists, there can be no freedom; when there is freedom there will be no State\" - Vladimir Lenin ");
+    }
+}
---
+++ BlockLetters.java
@@ -0,0 +1,17 @@
+public class BlockLetters {
+    public static void main(String[] args) {
+
+        System.out.println(" GG\n" + //
+                "G  \n" + //
+                "G GG\n" + //
+                "G  G\n" + //
+                "GGGG\n");
+        System.out.print("  SSS\n" + //
+                " SS\n" + //
+                "  S\n" + //
+                "  SS\n" + //
+                "SSS");
+
+
+    }
+}
