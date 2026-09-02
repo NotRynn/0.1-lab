b526ab71df0ff5143a4992cf585ff4f147c002ef
NotRynn <148168730+NotRynn@users.noreply.github.com>
2026-09-02 13:16:46 -0700

    dia

--- Diamond.java
+++ Diamond.java
@@ -1,7 +1,6 @@
 public class Diamond {
     public static void main(String[] args) {
-        System.out.print("\n" + //
-                "*\n" + //
+        System.out.println("*\n" + //
                 "***\n" + //
                 "*****\n" + //
                 "*******\n" + //
